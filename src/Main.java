f306f361274073e1f2115cb40adc8cae84439502
joel gamez <joelgamez14six@yahoo.com>
2018-07-21 18:40:09 -0400

    second commit to java practive with person object having an array of items

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args){
 
-        Person jojo = new Person("Jojo");
+        Person bob = new Person("bob");
 
         Scanner sc = new Scanner(System.in);
 
